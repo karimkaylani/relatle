ed5a8494a0b002ec7d3109b304248ddd5df7eda5
karimkaylani <karimkaylani@gmail.com>
2023-11-30 21:50:08 -0800

    Remove unused import

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
 import { promises as fs } from 'fs';
 import Game, { Artist } from './components/Game';
 
-import { Inter } from 'next/font/google'
 import Script from 'next/script';
- 
-// If loading a variable font, you don't need to specify the font weight
-const inter = Inter({ subsets: ['latin'] })
 
 export default async function Home() {
   const web = await getWeb()
