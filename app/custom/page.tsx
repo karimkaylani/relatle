5b38855ba06dbb7eef73c18475a01cb481921cd4
karimkaylani <karimkaylani@gmail.com>
2023-12-13 02:42:34 -0800

    Wrap GA and Game stuff in Suspense

--- app/custom/page.tsx
+++ app/custom/page.tsx
@@ -1,25 +1,28 @@
 import Script from 'next/script';
 import Game from '../components/Game';
 import { getWeb } from '../page';
+import { Suspense } from 'react';
 
 
 export default async function Home() {
     const web = await getWeb()
     return (
         <main>
-          <div className="container">
-          <Script async src="https://www.googletagmanager.com/gtag/js?id=G-J23EFVPLCJ"/>
-          <Script id="google-analytics">
-            {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-            
-              gtag('config', 'G-J23EFVPLCJ');
-            `}
-          </Script>
-        </div>
+        <Suspense>
+            <div className="container">
+            <Script async src="https://www.googletagmanager.com/gtag/js?id=G-J23EFVPLCJ"/>
+            <Script id="google-analytics">
+              {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+              
+                gtag('config', 'G-J23EFVPLCJ');
+              `}
+            </Script>
+            </div>
           <Game web={web} matchups={null} is_custom={true}/>
+        </Suspense>
         </main> 
     )
 }
