727a9c35e1686986e4c4f3805449e58049b77ac3
karimkaylani <karimkaylani@gmail.com>
2024-01-04 11:08:31 -0800

    Refactored artist card

--- app/components/ArtistCard.tsx
+++ app/components/ArtistCard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { Fragment, useEffect, useRef, useState } from 'react'
 import { Artist, phoneMaxWidth } from './Game'
 import { Card, Image, Text, Flex, BackgroundImage, RingProgress, Center, Transition, Overlay } from '@mantine/core';
 import { motion, useAnimate } from 'framer-motion'
@@ -107,31 +107,37 @@ const ArtistCard = ({artist, updateArtistHandler, path, won,
     })
   }
 
-  const artistCardContent = () => {
-    return (
-        <Card ref={scope}
-          shadow="sm" radius="md" withBorder
-          padding="xs" opacity={won && artist.name !== end ? 0.25 : 1}
-          styles={{
-            root: {
-              width: window.innerWidth > phoneMaxWidth ? "192px" : window.innerWidth > 345 ? "160px" : "",
-              userSelect: 'none', WebkitUserSelect: 'none', WebkitTouchCallout: 'none'
-            }
-          }}>
-            <Flex align="center" direction="column"
-              justify="center" gap="0px">
-                <Card.Section inheritPadding>
-                  <BackgroundImage draggable={false}
-                  radius="sm" src={artist.image} w={img_size} h={img_size}
-                  styles={{root: {userSelect: 'none', WebkitUserSelect: 'none', WebkitTouchCallout: 'none'}}}>
-                    {isPlaying && <Overlay backgroundOpacity={0.5}/>}
-                    <Center className={window.innerWidth > phoneMaxWidth ? 'pt-8' : 'pt-3'}>
-                    <Transition
-                      mounted={isPlaying}
-                      transition="fade"
-                      duration={400}
-                      timingFunction="ease">
-                      {(styles) => 
+  return (
+    <motion.button {...bind()}
+      whileHover={!clickable ? {} : window.innerWidth > phoneMaxWidth ? { scale: 1.05 } : {scale: 1.03}}
+      whileTap={{ scale: 0.95 }}
+      onTap={!clickable ? () => null : (() => won ? updateArtistHandler(artist) : clickArtistHandler())}
+      onTouchEnd={() => setLongPress(false)}
+      onMouseUp={() => setLongPress(false)}>
+      <Card ref={scope}
+        shadow="sm" radius="md" withBorder
+        padding="xs" opacity={won && artist.name !== end ? 0.25 : 1}
+        styles={{
+          root: {
+            width: window.innerWidth > phoneMaxWidth ? "192px" : window.innerWidth > 345 ? "160px" : "",
+            userSelect: 'none', WebkitUserSelect: 'none', WebkitTouchCallout: 'none'
+          }
+        }}>
+          <Flex align="center" direction="column"
+            justify="center" gap="0px">
+              <Card.Section inheritPadding>
+                <BackgroundImage draggable={false}
+                radius="sm" src={artist.image} w={img_size} h={img_size}
+                styles={{root: {userSelect: 'none', WebkitUserSelect: 'none', WebkitTouchCallout: 'none'}}}>
+                  <Center className={window.innerWidth > phoneMaxWidth ? 'pt-8' : 'pt-3'}>
+                  <Transition
+                    mounted={isPlaying}
+                    transition="fade"
+                    duration={400}
+                    timingFunction="ease">
+                    {(styles) =>
+                      <Fragment>
+                      <Overlay style={styles} backgroundOpacity={0.5}/>
                       <RingProgress thickness={5} style={styles} styles={{root: {zIndex: 1000}}}
                       sections={[{ value: progress, color: 'gray.1' }]}
                       label={
@@ -139,33 +145,22 @@ const ArtistCard = ({artist, updateArtistHandler, path, won,
                           <Lottie animationData={waveAnimation}
                           style={{width: 50}}/>
                         </Center>
-                      }/>}
-                    </Transition>
-                    </Center>
-                  </BackgroundImage>
-                
-                </Card.Section>
-                <Text c={path.includes(artist.name) && artist.name !== end ? "gray.5" : "gray.1"} fw={700} size={text_size} mt="md" ta="center"
-                styles={{root: {userSelect: 'none', WebkitUserSelect: 'none'}}}>
-                  {artist.name}
-                </Text>
-            </Flex>
-            <audio loop ref={audioRef} src={artist.top_song_preview_url}
-            onTimeUpdate={onTimeUpdate}/>
-        </Card>
-    ) 
-  }
-
-  return (
-    <motion.button {...bind()}
-        whileHover={!clickable ? {} : window.innerWidth > phoneMaxWidth ? { scale: 1.05 } : {scale: 1.03}}
-        whileTap={{ scale: 0.95 }}
-        onTap={!clickable ? () => null : (() => won ? updateArtistHandler(artist) : clickArtistHandler())}
-        onTouchEnd={() => setLongPress(false)}
-        onMouseUp={() => setLongPress(false)}>
-        {artistCardContent()}
+                      }/>
+                      </Fragment>}
+                  </Transition>
+                  </Center>
+                </BackgroundImage>
+              </Card.Section>
+              <Text c={path.includes(artist.name) && artist.name !== end ? "gray.5" : "gray.1"} fw={700} size={text_size} mt="md" ta="center"
+              styles={{root: {userSelect: 'none', WebkitUserSelect: 'none'}}}>
+                {artist.name}
+              </Text>
+          </Flex>
+          <audio loop ref={audioRef} src={artist.top_song_preview_url}
+          onTimeUpdate={onTimeUpdate}/>
+      </Card>
     </motion.button>
-  )
+  ) 
 }
 
 export default ArtistCard
