3ae083e1e6d74597606d687f9a010e7d53673c86
karimkaylani <karimkaylani@gmail.com>
2024-02-01 16:00:31 -0800

    Ensure streak doesn't reset if user jumps ahead and plays future game

--- app/components/Game.tsx
+++ app/components/Game.tsx
@@ -267,7 +267,7 @@ const Game = (props: GameProps) => {
             setWon(true)
             
             let new_streak = 1
-            if (prevMatchupID !== -1 && prevMatchupID === matchupID - 1) {
+            if (prevMatchupID !== -1 && prevMatchupID >= matchupID -1) {
                 new_streak = streak + 1
             }
             let new_longest_streak = new_streak > longestStreak ? new_streak : longestStreak
