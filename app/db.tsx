dbe2cc96c6ff77264fce1f3b65629b728aed4975
karimkaylani <karimkaylani@gmail.com>
2024-02-28 17:40:46 -0800

    Added log message

--- app/db.tsx
+++ app/db.tsx
@@ -4,6 +4,7 @@ import { unstable_cache } from "next/cache";
 
 const getAllGuesses = async (matchupID: number): Promise<any | null> => {
     const supabase = createClient();
+    console.log("Fetching guesses for matchup", matchupID)
     const { data, error } = await supabase.from('scores').select('guesses').eq('matchup_id', matchupID)
     if (error) {
       console.error(error)
