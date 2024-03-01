4e6a312ce3cf2575cd8743190e9785b68df34ec1
karimkaylani <karimkaylani@gmail.com>
2024-03-01 01:20:52 -0800

    Finish previous commit

--- app/db.tsx
+++ app/db.tsx
@@ -2,7 +2,7 @@
 import { createClient } from "@/utils/supabase/server";
 import { unstable_cache } from "next/cache";
 
-export const getAllGuesses = async (matchupID: number): Promise<any | null> => {
+const getAllGuesses = async (matchupID: number): Promise<any | null> => {
     const supabase = createClient();
     console.log("Fetching guesses for matchup", matchupID)
     const { data, error } = await supabase.from('scores').select('guesses').eq('matchup_id', matchupID)
