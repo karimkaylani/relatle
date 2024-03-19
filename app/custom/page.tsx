343c77d0e7607456ab6e2dbf5dd3ceb6b7202ad8
karimkaylani <karimkaylani@gmail.com>
2024-03-18 17:13:08 -0700

    Clean up layout, pages, update next

--- app/custom/page.tsx
+++ app/custom/page.tsx
@@ -1,34 +1,14 @@
-import Script from "next/script";
 import Game from "../components/Game";
-import { Suspense } from "react";
+import Web from "../../public/data/web.json";
 
 export const metadata = {
   title: "relatle (custom game)",
 };
 
 export default async function Home() {
-  const webReq = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/data/web.json`, { cache: "no-store"});
-  const Web = await webReq.json();
   return (
     <main>
-      <Suspense>
-        <div className="container">
-          <Script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=G-J23EFVPLCJ"
-          />
-          <Script id="google-analytics">
-            {`
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-              
-                gtag('config', 'G-J23EFVPLCJ');
-              `}
-          </Script>
-        </div>
-        <Game web={Web} is_custom={true} />
-      </Suspense>
+      <Game web={Web} is_custom={true} />
     </main>
   );
 }
