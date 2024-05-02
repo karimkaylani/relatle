c55549af0baf9b9e8cd564febf2079d3186195a5
karimkaylani <karimkaylani@gmail.com>
2024-05-01 20:12:40 -0700

    Clean up stats component

--- app/components/GlobalScoreStats.tsx
+++ app/components/GlobalScoreStats.tsx
@@ -6,7 +6,7 @@ import ScoreHistogram from "./ScoreHistogram";
 
 export interface GlobalScoreStatsProps {
   guesses: number;
-  stats: Stats|null;
+  stats: Stats;
   won: boolean;
   shortestPath: number;
 }
@@ -14,50 +14,48 @@ export interface GlobalScoreStatsProps {
 const Stat = (props: { label: string; value: string }) => {
   return (
     <Group justify="space-between">
-      <Text ta="center" size='sm'>
+      <Text ta="center" size="sm">
         {props.label}
       </Text>
-      <Text ta="center" c={white} fw={700} size='sm'>
+      <Text ta="center" c={white} fw={700} size="sm">
         {props.value}
       </Text>
     </Group>
   );
-}
+};
 
 const GlobalScoreStats = (props: GlobalScoreStatsProps) => {
   const { guesses, stats, won, shortestPath } = props;
   return (
     <Stack align="center" justify="center">
+       <Text fw={700} ta="center" size="sm" c={white}>
+        {"Score Distribution"}
+      </Text>
+      <ScoreHistogram bins={stats.bins} guesses={guesses} won={won} />
       <Text fw={700} ta="center" size="sm" c={white}>
         {"Global Matchup Stats"}
       </Text>
-      {!stats ? (
-        <Text ta="center" size="sm" c={white}>
-          Come back soon for global results
-        </Text>
-      ) : (
-        <>
-          <Group justify='center' align="center" gap='35px'>
-            <Stack>
-                <Stat label={"Shortest Path"} value={shortestPath.toString()} />
-                <Stat label={"Your Score"} value={guesses.toString()} />
-                <Stat label={"Average Score"} value={stats.averageScore.toFixed(0)} />
-            </Stack>
-            <Stack>
-                <Stat label={"Number of Plays"} value={stats.numGames.toLocaleString()} />
-                <Stat label={"Perfect Game Rate"} value={stats.perfectGameRate.toFixed(0) + "%"} />
-                <Stat label={"Win Rate"} value={stats.winRate.toFixed(0) + "%"} />
-            </Stack>
-          </Group>
-          <Text fw={700} ta="center" size="sm" c={white}>
-            {"Score Distribution"}
-          </Text>
-          <ScoreHistogram bins={stats.bins} guesses={guesses} won={won}/>
-          <Text ta="center" size="sm">
-            These values will update every couple of minutes
-          </Text>
-        </>
-      )}
+      <Group justify="center" align="center" gap="35px">
+        <Stack>
+          <Stat label={"Shortest Path"} value={shortestPath.toString()} />
+          <Stat label={"Your Score"} value={guesses.toString()} />
+          <Stat label={"Average Score"} value={stats.averageScore.toFixed(0)} />
+        </Stack>
+        <Stack>
+          <Stat
+            label={"Number of Plays"}
+            value={stats.numGames.toLocaleString()}
+          />
+          <Stat
+            label={"Perfect Game Rate"}
+            value={stats.perfectGameRate.toFixed(0) + "%"}
+          />
+          <Stat label={"Win Rate"} value={stats.winRate.toFixed(0) + "%"} />
+        </Stack>
+      </Group>
+      <Text ta="center" size="sm" fs='italic' c={'#F1F3F5'}>
+        These values will update every couple of minutes
+      </Text>
     </Stack>
   );
 };
