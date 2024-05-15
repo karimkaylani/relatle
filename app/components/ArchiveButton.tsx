bc63547f728e44dacd369fca6e1ffa63a87de9a0
karimkaylani <karimkaylani@gmail.com>
2024-05-15 00:27:54 -0700

    Use router instead of window.open

--- app/components/ArchiveButton.tsx
+++ app/components/ArchiveButton.tsx
@@ -1,18 +1,28 @@
-import { IconHistory } from '@tabler/icons-react'
-import React from 'react'
-import { white } from '../colors'
-import { maxCustomTextWidth } from './Game'
-import IconHoverButton from './IconHoverButton'
+import { IconHistory } from "@tabler/icons-react";
+import React from "react";
+import { white } from "../colors";
+import { maxCustomTextWidth } from "./Game";
+import IconHoverButton from "./IconHoverButton";
+import { useRouter } from "next/navigation";
 
 export interface ArchiveButtonProps {
-    showText?: boolean;
-    text?: string;
+  showText?: boolean;
+  text?: string;
 }
 
-const ArchiveButton = ({showText=true, text='Archive'}: ArchiveButtonProps) => {
+const ArchiveButton = ({
+  showText = true,
+  text = "Archive",
+}: ArchiveButtonProps) => {
+  const router = useRouter();
   return (
-    <IconHoverButton onTap={() => window.open('/archive', '_self')} icon={<IconHistory color={white} size={24} />} showText={showText} text={text} />
-  )
-}
+    <IconHoverButton
+      onTap={() => router.push("/archive")}
+      icon={<IconHistory color={white} size={24} />}
+      showText={showText}
+      text={text}
+    />
+  );
+};
 
-export default ArchiveButton
+export default ArchiveButton;
--- app/components/TopGamesButton.tsx
+++ app/components/TopGamesButton.tsx
@@ -1,18 +1,28 @@
-import React from 'react'
-import IconHoverButton from './IconHoverButton'
-import { IconTrophy } from '@tabler/icons-react'
-import { maxCustomTextWidth } from './Game'
-import { white } from '../colors'
+import React from "react";
+import IconHoverButton from "./IconHoverButton";
+import { IconTrophy } from "@tabler/icons-react";
+import { maxCustomTextWidth } from "./Game";
+import { white } from "../colors";
+import { useRouter } from "next/navigation";
 
 export interface TopGamesButtonProps {
-    showText?: boolean;
-    text?: string;
+  showText?: boolean;
+  text?: string;
 }
 
-const TopGamesButton = ({showText=true, text='Top Games'}: TopGamesButtonProps) => {
+const TopGamesButton = ({
+  showText = true,
+  text = "Top Games",
+}: TopGamesButtonProps) => {
+  const router = useRouter();
   return (
-    <IconHoverButton onTap={() => window.open('top-games', '_self')} icon={<IconTrophy color={white} size={24} />} showText={showText} text={text} />
-  )
-}
+    <IconHoverButton
+      onTap={() => router.push("top-games")}
+      icon={<IconTrophy color={white} size={24} />}
+      showText={showText}
+      text={text}
+    />
+  );
+};
 
-export default TopGamesButton
+export default TopGamesButton;
