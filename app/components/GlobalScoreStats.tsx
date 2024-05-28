6a996a232af115d2cc966ef5590282efd32b039b
karimkaylani <karimkaylani@gmail.com>
2024-05-28 09:01:40 -0700

    Remove text from global score stats section

--- app/components/GlobalScoreStats.tsx
+++ app/components/GlobalScoreStats.tsx
@@ -53,9 +53,6 @@ const GlobalScoreStats = (props: GlobalScoreStatsProps) => {
           />
         </Stack>
       </Group>
-      <Text ta="center" size="sm">
-        These values will update as the day progresses
-      </Text>
     </Stack>
   );
 };
