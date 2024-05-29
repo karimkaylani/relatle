ee6740204a5d4985866b08401abda70a1691f56a
karimkaylani <karimkaylani@gmail.com>
2024-05-29 11:14:19 -0700

    Fix missing safe area for PWAs

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,6 +51,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover" />
         <ColorSchemeScript defaultColorScheme="dark" />
       </head>
       <body>
